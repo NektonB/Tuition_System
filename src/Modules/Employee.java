f465e7c760fd750b031e003cfd1fd062315d86e4
Buddhika Prasanna <kbuddhika89@gmail.com>
2020-07-27 14:11:37 +0530

    Update Modules

--- src/Modules/Employee.java
+++ src/Modules/Employee.java
@@ -1,4 +1,58 @@
 package Modules;
 
 public class Employee {
+
+    private int id = 0;
+    private String fname = "";
+    private String lname = "";
+    private String nic_number = "";
+    private String address = "";
+
+    public void resetAll() {
+        id = 0;
+        fname = "";
+        lname = "";
+        nic_number = "";
+        address = "";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getNic_number() {
+        return nic_number;
+    }
+
+    public void setNic_number(String nic_number) {
+        this.nic_number = nic_number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
--- src/Modules/Status.java
+++ src/Modules/Status.java
@@ -2,4 +2,27 @@ package Modules;
 
 public class Status {
 
+    private int id = 0;
+    private String status = "";
+
+    public void resetAll() {
+        setId(0);
+        setStatus("");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
