43190bbaa6c5e6434e92613b6f0166d0bce7c7a7
Buddhika Prasanna <kbuddhika89@gmail.com>
2020-09-30 09:44:17 +0530

    Update Attendance Report

--- src/Controllers/ReportViewer.java
+++ src/Controllers/ReportViewer.java
@@ -4,6 +4,7 @@ import DB_Conn.ConnectDB;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.view.JasperViewer;
 
+import javax.swing.*;
 import java.awt.*;
 import java.sql.Connection;
 import java.util.HashMap;
@@ -31,19 +32,16 @@ public class ReportViewer {
             parameter.put("Date", Date);
             String[] name = TeacherName.split(" ");
             parameter.put("TeacherFname", name[0]);
-            parameter.put("TeacherFname", name[1]);
+            parameter.put("TeacherLname", name[1]);
 
             JasperPrint printIt = JasperFillManager.fillReport(RI, parameter, conn);
 
             if (viewType.equals("PRINT")) {
                 JasperPrintManager.printReport(printIt, false);
-                //JasperPrintManager.printReport(printIt, false);
             } else if (viewType.equals("VIEW")) {
-                JasperViewer.viewReport(printIt, false);
-                /*try {
+                //JasperViewer.viewReport(printIt, false);
+                try {
                     UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                    //JasperViewer viewer = new JasperViewer(printIt, false);
-                    //viewer.setVisible(true);
                     JasperViewer.viewReport(printIt, false);
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
@@ -53,112 +51,7 @@ public class ReportViewer {
                     e.printStackTrace();
                 } catch (UnsupportedLookAndFeelException e) {
                     e.printStackTrace();
-                }*/
-            }
-        } catch (Exception e) {
-            Toolkit.getDefaultToolkit().beep();
-            e.printStackTrace();
-
-        }
-    }
-
-    public void getCreditInvoice(String invoiceId, String viewType) {
-        try {
-            String path = "C:\\Program Files\\Common Files\\ICSv2\\Reports\\Credit_Invoice.jrxml";
-            JasperReport RI = JasperCompileManager.compileReport(path);
-            Map<String, Object> parameter = new HashMap<>();
-            parameter.put("InvoiceNumber", invoiceId);
-            JasperPrint printIt = JasperFillManager.fillReport(RI, parameter, conn);
-            if (viewType.equals("PRINT")) {
-                JasperPrintManager.printReport(printIt, false);
-                //JasperPrintManager.printReport(printIt, false);
-            } else if (viewType.equals("VIEW")) {
-                JasperViewer.viewReport(printIt, false);
-            }
-        } catch (Exception e) {
-            Toolkit.getDefaultToolkit().beep();
-            e.printStackTrace();
-
-        }
-    }
-
-    public void getStockLogByStockDates(String startDate, String endDate, String viewType) {
-        try {
-            String path = "C:\\Program Files\\Common Files\\ICSv2\\Reports\\StockLogByStockDate.jrxml";
-            JasperReport RI = JasperCompileManager.compileReport(path);
-            Map<String, Object> parameter = new HashMap<>();
-            parameter.put("startDate", startDate);
-            parameter.put("endDate", endDate);
-            JasperPrint printIt = JasperFillManager.fillReport(RI, parameter, conn);
-            if (viewType.equals("PRINT")) {
-                JasperPrintManager.printReport(printIt, false);
-                //JasperPrintManager.printReport(printIt, false);
-            } else if (viewType.equals("VIEW")) {
-                JasperViewer.viewReport(printIt, false);
-            }
-        } catch (Exception e) {
-            Toolkit.getDefaultToolkit().beep();
-            e.printStackTrace();
-
-        }
-    }
-
-    public void getDailyInvoiceSummary(String invoicedDate, String viewType) {
-        try {
-            String path = "C:\\Program Files\\Common Files\\ICSv2\\Reports\\DailyInvoiceSummary.jrxml";
-            JasperReport RI = JasperCompileManager.compileReport(path);
-            Map<String, Object> parameter = new HashMap<>();
-            parameter.put("invoicedDate", invoicedDate);
-
-            JasperPrint printIt = JasperFillManager.fillReport(RI, parameter, conn);
-            if (viewType.equals("PRINT")) {
-                JasperPrintManager.printReport(printIt, false);
-                //JasperPrintManager.printReport(printIt, false);
-            } else if (viewType.equals("VIEW")) {
-                JasperViewer.viewReport(printIt, false);
-            }
-        } catch (Exception e) {
-            Toolkit.getDefaultToolkit().beep();
-            e.printStackTrace();
-
-        }
-    }
-
-    public void getDailyUserInvoiceSummary(String invoicedDate, String userName, String viewType) {
-        try {
-            String path = "C:\\Program Files\\Common Files\\ICSv2\\Reports\\DailyUserInvoiceSummary.jrxml";
-            JasperReport RI = JasperCompileManager.compileReport(path);
-            Map<String, Object> parameter = new HashMap<>();
-            parameter.put("invoicedDate", invoicedDate);
-            parameter.put("userName", userName);
-
-            JasperPrint printIt = JasperFillManager.fillReport(RI, parameter, conn);
-            if (viewType.equals("PRINT")) {
-                JasperPrintManager.printReport(printIt, false);
-                //JasperPrintManager.printReport(printIt, false);
-            } else if (viewType.equals("VIEW")) {
-                JasperViewer.viewReport(printIt, false);
-            }
-        } catch (Exception e) {
-            Toolkit.getDefaultToolkit().beep();
-            e.printStackTrace();
-
-        }
-    }
-
-    public void getDailySaleSummary(String invoicedDate, String viewType) {
-        try {
-            String path = "C:\\Program Files\\Common Files\\ICSv2\\Reports\\SaleReport.jrxml";
-            JasperReport RI = JasperCompileManager.compileReport(path);
-            Map<String, Object> parameter = new HashMap<>();
-            parameter.put("saleDate", invoicedDate);
-
-            JasperPrint printIt = JasperFillManager.fillReport(RI, parameter, conn);
-            if (viewType.equals("PRINT")) {
-                JasperPrintManager.printReport(printIt, false);
-                //JasperPrintManager.printReport(printIt, false);
-            } else if (viewType.equals("VIEW")) {
-                JasperViewer.viewReport(printIt, false);
+                }
             }
         } catch (Exception e) {
             Toolkit.getDefaultToolkit().beep();
